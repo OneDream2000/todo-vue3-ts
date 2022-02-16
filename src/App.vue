c9fce1437a582a3537cec8c6cd2f3df73f2d6c55
jason <1207436809@qq.com>
2022-02-16 10:02:58 +0800

    列表展示功能

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
-<script setup>
+<script setup lang="ts">
 import TodoHeader from './components/TodoHeader.vue'
 import TodoMain from './components/TodoMain.vue'
 import TodoFooter from './components/TodoFooter.vue'
+import { ref } from 'vue';
+export interface listType {
+    id: number,
+    name: string,
+    done: boolean,
+}
+// 提供数据
+const list = ref<listType[]>([
+  {
+    id: 1,
+    name: '吃饭',
+    done: true,
+  },
+  {
+    id: 2,
+    name: '睡觉',
+    done: false,
+  },
+  {
+    id: 3,
+    name: '打豆豆',
+    done: false,
+  },
+])
 </script>
 
 <template>
   <section class="todoapp">
     <TodoHeader></TodoHeader>
-    <TodoMain></TodoMain>
+    <TodoMain :list="list"></TodoMain>
     <TodoFooter></TodoFooter>
   </section>
 </template>
--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -1,25 +1,25 @@
-<script setup></script>
+<script setup lang="ts">
+import { listType } from '../App.vue';
+
+interface Props{
+  list:listType[]
+}
+const {list:[]} = defineProps<Props>()
+
+</script>
 
 <template>
   <section class="main">
     <input id="toggle-all" class="toggle-all" type="checkbox" />
     <label for="toggle-all">Mark all as complete</label>
     <ul class="todo-list">
-      <li class="completed">
+      <li :class="{completed:item.done}" v-for="item in list" :key="item.id">
         <div class="view">
-          <input class="toggle" type="checkbox" checked />
-          <label>Taste JavaScript</label>
+          <input class="toggle" type="checkbox" :checked="item.done"/>
+          <label>{{item.name}}</label>
           <button class="destroy"></button>
         </div>
         <input class="edit" value="Create a TodoMVC template" />
-      </li>
-      <li>
-        <div class="view">
-          <input class="toggle" type="checkbox" />
-          <label>Buy a unicorn</label>
-          <button class="destroy"></button>
-        </div>
-        <input class="edit" value="Rule the web" />
       </li>
     </ul>
   </section>
