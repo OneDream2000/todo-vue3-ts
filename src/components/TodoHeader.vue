4ba47c83cb585a62435ff59d9761b2b85346f34b
jason <1207436809@qq.com>
2022-02-16 20:31:35 +0800

    添加任务

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import useStore from '../store'
+const {todos} =useStore()
+  const todoName = ref('')
+  const addTodo = () => { 
+    if(!todoName.value) return alert('待办不能为空')
+    todos.addTodo(todoName.value)
+    todoName.value =''
+   }
+</script>
 
 <template>
   <header class="header">
     <h1>todos</h1>
-    <input class="new-todo" placeholder="What needs to be done?" autofocus />
+    <input class="new-todo" v-model="todoName" @keyup.enter="addTodo" placeholder="What needs to be done?" autofocus />
   </header>
 </template>
 
--- src/store/modules/todos.ts
+++ src/store/modules/todos.ts
@@ -1,7 +1,7 @@
-import {defineStore} from 'pinia'
+import { defineStore } from 'pinia';
 
-const useTodosStore = defineStore('todos',{
-    // 状态
+const useTodosStore = defineStore('todos', {
+  // 状态
   state() {
     return {
       list: [
@@ -9,23 +9,32 @@ const useTodosStore = defineStore('todos',{
         { id: 2, content: '睡觉', done: false },
         { id: 3, content: '打代码', done: false },
       ],
-    }
+    };
   },
   // 计算
   getters: {},
   // 函数
   actions: {
-     // 修改任务状态
-    changeDone(id:number){
-      const todo = this.list.find(item=>item.id === id)
+    // 修改任务状态
+    changeDone(id: number) {
+      const todo = this.list.find((item) => item.id === id);
       if (todo) {
-        todo.done = !todo.done
+        todo.done = !todo.done;
       }
     },
-    delTodo(id:number){
-      this.list = this.list.filter(item=>item.id!==id)
-    }
+    // 删除
+    delTodo(id: number) {
+      this.list = this.list.filter((item) => item.id !== id);
+    },
+    // 添加
+    addTodo(content: string) {
+      this.list.unshift({
+        id: Date.now(),
+        content,
+        done: false,
+      });
+    },
   },
-})
+});
 
-export default useTodosStore
+export default useTodosStore;
